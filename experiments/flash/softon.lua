fc411cdd033cdff2dd2953c7149affc06cb5e7a5
Ard van Breemen <ard@kwaak.net>
2017-09-03 14:27:46 +0200

    hack some path in flashit. encode parameters into scripts (loadfile())(parameters...)

--- experiments/flash/softon.lua
+++ experiments/flash/softon.lua
@@ -1,8 +1,10 @@
+local arg=...
+local config=require"config"
 ws2812.init()
 local timer=tmr.create()
 local softon=coroutine.wrap(function()
-	local buffer=ws2812.newBuffer(600,4)
-	for i=1,100 do
+	local buffer=ws2812.newBuffer(config.pixels or 300,config.ledpp or 4)
+	for i=1,arg.brightness or 100 do
 		buffer:fill(0,0,0,i)
 		coroutine.yield()
 		ws2812.write(buffer)
@@ -10,4 +12,4 @@ local softon=coroutine.wrap(function()
 	timer:stop()
 	timer:unregister()
 end)
-timer:alarm(100,tmr.ALARM_AUTO,softon)
+timer:alarm(arg.interval or 100,tmr.ALARM_AUTO,softon)

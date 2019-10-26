ec219b7a3ba87d29f8b8b30a62d85c69aa475019
Ard van Breemen <ard@kwaak.net>
2019-10-26 15:36:02 +0200

    test animations

--- experiments/mqtt/test4.sh
+++ experiments/mqtt/test4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-N=(255,0,0,0 0,255,0,0 0,0,255,0 0,0,0,255 128,128,0,0 0,128,128,0 128,0,128,0 0,0,0,0 64,64,64,64 128,0,0,128 0,128,0,128 0,0,128,128)
+N=(255,0,0,0 0,255,0,0 0,0,255,0 0,0,0,255 128,128,0,0 0,128,128,0 128,0,128,0 64,64,64,64 128,0,0,128 0,128,0,128 0,0,128,128)
 echo ${#N[*]}
 NR=${#N[*]}
 selectcolor() {
@@ -15,6 +15,8 @@ C=$(selectcolor)
 done
 OC="$C"
 ./sendm.lua 'do ws2812.init() local B=ws2812.newBuffer(300,4) B:fill('$C') ws2812.write(B) end' /esp/a0:20:a6:04:{05:1f,03:b7,03:76,01:c8,02:d2,02:e6}/lua
-sleep .4
+sleep .1
+./sendm.lua 'do ws2812.init() local B=ws2812.newBuffer(300,4) B:fill(0,0,0,0) ws2812.write(B) end' /esp/a0:20:a6:04:{05:1f,03:b7,03:76,01:c8,02:d2,02:e6}/lua
+sleep .3
 done
 #while true;do for j in 255;do for i in "$j,0,0,0" "0,$j,0,0" "0,0,$j,0" "0,0,0,$j";do ./sendm.lua 'do ws2812.init() local B=ws2812.newBuffer(300,4) B:fill('$i') ws2812.write(B) end' /esp/a0:20:a6:04:{05:1f,03:b7,03:76,01:c8,02:d2}/lua  ;sleep 0.4;done;done;done
